d86d3eda539e72524d2fe2fd563b499774baa74f
Vitor <dev_vitorsouza@hotmail.com>
2019-07-11 12:14:23 -0300

    5.8. Desafio_ Removendo lançamentos

--- src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoneyapi.event.RecursoCriadoEvent;
@@ -43,6 +45,11 @@ public class LancamentoResource {
 		Lancamento l = lancamentoRepository.save(lancamento);	
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, l.getCodigo())); 
 		return ResponseEntity.status(HttpStatus.CREATED).body(l);
+	}
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long codigo) {
+		lancamentoRepository.delete(codigo);
 	}
 
 	
